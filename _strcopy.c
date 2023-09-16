1a528e0f8e1db10e8bd8118d2784aac7b72f89bd
Mohamed <muhammadamin1916@gmail.com>
2023-09-16 13:59:36 -0700

    edit

---
+++ _strcopy.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+/**
+ * _strcpy - Copies a source string into a destination string.
+ * @cpy: The destination string.
+ * @orignal: The source string.
+ * Return: A pointer to the destination string.
+ */
+char* _strcpy(char* cpy, const char* orignal)
+{
+	int i = 0;
+	char* cpy_ptr = cpy;
+	const char* orignal_ptr = orignal;
+
+	while ((*cpy_ptr++ = *orignal_ptr++) != '\0')
+	{
+		cpy[i] = orignal[i];
+		i++;
+	}
+	return (cpy);
+}
---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * _strlen - A function that calculates the length of string.
+ * @str: The string.
+ * Return: The length of the string.
+ */
+size_t _strlen(const char* str)
+{
+	size_t length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
