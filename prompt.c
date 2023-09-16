a03c0976f23aac8f3f6a1a511248f4e8298167f5
Mohamed <muhammadamin1916@gmail.com>
2023-09-16 10:58:01 -0700

    prompt

---
+++ prompt.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+ * display_prompt = A function that displays a prompt for user.
+ */
+
+void display_prompt()
+{
+	ssize_t value = 0;
+	if (isatty(STDIN_FILENO))
+	{
+		value = write(STDOUT_FILENO, "$ ", 2);
+		if (value == -1)
+		{
+			perror("write");
+			exit(EXIT_FAILURE);
+		}
+}
